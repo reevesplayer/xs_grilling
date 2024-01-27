3b449b3672db361ca99d9946d21797119121737c
Cryptic <reevesplayer@outlook.com>
2024-01-27 01:28:18 -0500

    Added: Base file structure for script

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Cryptic @ X-Studios'
+description 'X-Studios Grilling Script'
+version '1.0.0'
+
+server_scripts {
+    'server/main.lua',
+}
+
+client_scripts {
+    'client/main.lua',
+}
+
+shared_scripts {
+    'config.lua',
+    -- @ox_lib/init.lua
+}
